8604a6bc40862584b3aee3bb7aec50939ba92b61
dimadawid <dimadawid@mail.ru>
2022-12-28 18:44:05 +0300

    fotohosting

---
+++ function.php
@@ -0,0 +1,149 @@
+<?php
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+function registration(): bool
+{
+    global $pdo;
+
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if (empty($login) || empty($pass)) {
+        $_SESSION['errors'] = 'Поля логин/пароль обязательны';
+        return false;
+    }
+
+    $res = $pdo->prepare("SELECT COUNT(*) FROM users WHERE login = ?");
+    $res->execute([$login]);
+    if ($res->fetchColumn()) {
+        $_SESSION['errors'] = 'Данное имя уже используется';
+        return false;
+    }
+
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    $res = $pdo->prepare("INSERT INTO users (login, pass) VALUES (?,?)");
+    if ($res->execute([$login, $pass])) {
+        $_SESSION['success'] = 'Успешная регистрация';
+        return true;
+    } else {
+        $_SESSION['errors'] = 'Ошибка регистрации';
+        return false;
+    }
+}
+
+function login(): bool
+{
+    global $pdo;
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if (empty($login) || empty($pass)) {
+        $_SESSION['errors'] = 'Поля логин/пароль обязательны';
+        return false;
+    }
+
+    $res = $pdo->prepare("SELECT * FROM users WHERE login = ?");
+    $res->execute([$login]);
+    if (!$user = $res->fetch()) {
+        $_SESSION['errors'] = 'Логин/пароль введены неверно';
+        return false;
+    }
+
+    if (!password_verify($pass, $user['pass'])) {
+        $_SESSION['errors'] = 'Логин/пароль введены неверно';
+        return false;
+    } else {
+        $_SESSION['success'] = 'Вы успешно авторизовались';
+        $_SESSION['user']['name'] = $user['login'];
+        $_SESSION['user']['id'] = $user['id'];
+        return true;
+    }
+}
+
+function save_message(): bool
+{
+    global $pdo;
+    $message = !empty($_POST['message']) ? trim($_POST['message']) : '';
+
+    if (!isset($_SESSION['user']['name'])) {
+        $_SESSION['errors'] = 'Необходимо авторизоваться';
+        return false;
+    }
+
+    if (empty($message)) {
+        $_SESSION['errors'] = 'Введите текст сообщения';
+        return false;
+    }
+
+    $res = $pdo->prepare("INSERT INTO messages (name, message) VALUES (?,?)");
+    if ($res->execute([$_SESSION['user']['name'], $message])) {
+        $_SESSION['success'] = 'Сообщение добавлено';
+        return true;
+    } else {
+        $_SESSION['errors'] = 'Ошибка!';
+        return false;
+    }
+}
+
+
+function save_img(): bool
+{
+    global $pdo;
+
+
+    if (!isset($_SESSION['user']['name'])) {
+        $_SESSION['errors'] = 'Необходимо авторизоваться';
+        return false;
+    }
+
+
+    if (isset($_POST["addimg"])) {
+
+        // Count total files
+        $countfiles = count($_FILES["files"]["name"]);
+        // Prepared statement
+        $res = $pdo->prepare("INSERT INTO images (name,image) VALUES(?,?)");
+        // Loop all files
+        for ($i = 0; $i < $countfiles; $i++) {
+            // File name
+            $filename = $_FILES["files"]["name"][$i];
+            // Location
+            $target_file = "./uploads/" . $filename;
+            // file extension
+            $file_extension = pathinfo($target_file, PATHINFO_EXTENSION);
+            $file_extension = strtolower($file_extension);
+            // Valid image extension
+            $valid_extension = array(
+                "png",
+                "jpeg",
+                "jpg"
+            );
+            if (in_array($file_extension, $valid_extension)) {
+                // Upload file
+                if (move_uploaded_file($_FILES["files"]["tmp_name"][$i], $target_file)) {
+                    // Execute query
+
+
+                    $res->execute(
+                        array($filename, $target_file)
+                    );
+                }
+            }
+        }
+
+        $_SESSION['success'] = 'Успешная загрузка';
+        return true;
+    }
+}
+
+
+function get_messages(): array
+{
+    global $pdo;
+    $res = $pdo->query("SELECT * FROM messages");
+    return $res->fetchAll();
+}
