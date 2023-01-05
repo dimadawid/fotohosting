4d62df9448c526973e5a7e4ff47d98d494514ac0
dimadawid <dimadawid@mail.ru>
2023-01-05 14:46:43 +0300

    correction file

--- comment.php
+++ comment.php
@@ -25,48 +25,70 @@ $messages = get_messages();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Фотохостинг</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-<div class="container my-4">
-<h1>Описание и фото</h1>
-<div class="row">
-                <div class="col-md-12 text-center my-4">
-                    <a href="index.php"><button class="btn btn-primary">На Главную</button></a>
+
+    <div class="header">
+        <div class="row">
+
+            <div class="col-md-6">
+                <div class="logo">
+                    <img src="./img/logofoto.png" alt="">
+
                 </div>
-
-
-
             </div>
+            <div class="col-md-6 align-self-center">
+                <div class="nav">
+                    <ul>
+                        <li><a href="index.php">главная</a>
+                        <li>о нас</li>
+                        <li><a href="comment.php">все фото</a></li>
 
-
-    <div class="row">
-
-     
-        <div class="col-md-4">
-
-
-        <?php
-     
-     $stmt = $pdo->prepare("select * from images");
-     $stmt->execute();
-     $imagelist = $stmt->fetchAll();
-   
-     foreach($imagelist as $image) {
-     ?>
-         
-     <img src="<?=$image["image"]?>"
-         title="<?=$image["name"] ?>"
-         width="350" height="350">
-     <?php
-     }
-     ?>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 
+    <div class="container my-4">
+        <h1>фото</h1>
+        <!-- <div class="row">
+            <div class="col-md-12 text-center my-4">
+                <a href="index.php"><button class="btn btn-primary">На Главную</button></a>
+            </div>
 
 
-</div>
+
+        </div> -->
+
+
+        <div class="row my-4">
+
+
+            <div class="col-md-4">
+
+
+                <?php
+
+                $stmt = $pdo->prepare("select * from images");
+                $stmt->execute();
+                $imagelist = $stmt->fetchAll();
+
+                foreach ($imagelist as $image) {
+                ?>
+
+                    <img src="<?= $image["image"] ?>" title="<?= $image["name"] ?>" width="350" height="350">
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+
+
+
+    </div>
 
 </body>
 
--- index.php
+++ index.php
@@ -78,11 +78,38 @@ if (isset($_GET['delimg'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Фотохостинг</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
+    <div class="header">
+        <div class="row">
+
+            <div class="col-md-6">
+                <div class="logo">
+                    <img src="./img/logofoto.png" alt="">
+
+                </div>
+            </div>
+            <div class="col-md-6 align-self-center">
+                <div class="nav">
+                    <ul>
+                        <li><a href="index.php">главная</a></li>
+                        <li>о нас</li>
+                        <li><a href="comment.php">все фото</a></li>
+
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     <div class="container">
+
+
+
 
         <div class="row my-4">
             <?php if (!empty($_SESSION['errors'])) : ?>
@@ -171,7 +198,7 @@ if (isset($_GET['delimg'])) {
 
                 <div class="row">
                     <div class="col-md-6 offset-md-3">
-                        <p>Добро пожаловать, <?= htmlspecialchars($_SESSION['user']['name']) ?>! <a href="?do=exit">Выйти</a></p>
+                        <p>Добро пожаловать, <?= htmlspecialchars($_SESSION['user']['name']) ?>! <a  class="delbutton" href="?do=exit">Выйти</a></p>
                     </div>
                 </div>
                 <form action="index.php" method="post" class="row g-3 mb-5">
@@ -200,7 +227,7 @@ if (isset($_GET['delimg'])) {
 
                 </form>
 
-                
+
 
 
 
@@ -215,7 +242,7 @@ if (isset($_GET['delimg'])) {
                                         <h5 class="card-title">Автор: <?= htmlspecialchars($message['name']) ?></h5>
                                         <p class="card-text"><?= htmlspecialchars($message['message']) ?></p>
                                         <p>Дата: <?= $message['created_at'] ?></p>
-                                        <a href="?del=<?= $message['id'] ?>">Удалить</a>
+                                        <a class="delbutton" href="?del=<?= $message['id'] ?>">Удалить</a>
                                         <!-- <a href="?red=<?= $message['id'] ?>">Редактировать</a> -->
                                     </div>
                                 </div>
@@ -227,31 +254,32 @@ if (isset($_GET['delimg'])) {
 
                         </div>
 
-            
+
                     </div>
                 <?php endif; ?>
-            <?php endif; ?>
-            <?php
-                $stmt = $pdo->prepare("select * from images");
-                $stmt->execute();
-                $imagelist = $stmt->fetchAll();
-                ?>
+                <?php
+            $stmt = $pdo->prepare("select * from images");
+            $stmt->execute();
+            $imagelist = $stmt->fetchAll();
+            ?>
 
-                <?php foreach ($imagelist as $image) : ?>
-                    <div class="col-md-4 text-center my-4">
-                        <img src="<?= $image["image"] ?>" title="<?= $image["name"] ?>" width="350" height="350">
-                        <a href="?delimg=<?= $image['id'] ?>">Удалить</a>
-                    </div>
-                <?php endforeach; ?>
+            <?php foreach ($imagelist as $image) : ?>
+                <div class="col-md-4 text-center my-4">
+                    <img src="<?= $image["image"] ?>" title="<?= $image["name"] ?>" width="350" height="350">
+                    <a class="delbutton" href="?delimg=<?= $image['id'] ?>">Удалить</a>
+                </div>
+            <?php endforeach; ?>
+            <?php endif; ?>
+      
             </br>
-            <div class="row">
+            <!-- <div class="row">
                 <div class="col-md-12 text-center my-4">
                     <a href="comment.php"><button class="btn btn-primary">Смотреть все фото</button></a>
                 </div>
 
 
 
-            </div>
+            </div> -->
 
 
         </div>
